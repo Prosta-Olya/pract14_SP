3c6b0dc3dc8032384e4a22addbd317d912ae0d00
Olya <siverolya@gmail.com>
2026-04-08 17:51:01 +0500

    готово

--- pract14.cpp
+++ pract14.cpp
@@ -31,39 +31,32 @@ struct ClubState {
 	LONG timeoutCount; // Количество ушедших по таймауту
 };
 
-// --- Глобальные переменные ---
 ClubState g_clubState = { 0 };
 HANDLE g_hSemaphore = NULL;
 CRITICAL_SECTION g_csState; // Для защиты данных состояния
 BOOL g_bAllVisitorsFinished = FALSE;
 
-// --- Прототипы функций ---
 DWORD WINAPI VisitorThreadProc(LPVOID lpParam);
 DWORD WINAPI ObserverThreadProc(LPVOID lpParam);
 void PrintFinalStats();
 
-// --- Основной поток ---
 int main() {
     setlocale(LC_ALL, "Russian");
-    // Инициализация генератора случайных чисел
     srand((unsigned int)time(NULL));
 
-    // Инициализация критической секции
     InitializeCriticalSection(&g_csState);
 
-    // Инициализация семафора
 #if USE_SEMAPHORE
     g_hSemaphore = CreateSemaphore(NULL, CLUB_CAPACITY, CLUB_CAPACITY, NULL);
     if (g_hSemaphore == NULL) {
         std::cout << "Ошибка создания семафора: " << GetLastError() << std::endl;
         return 1;
     }
-    std::cout << "=== ЗАПУСК В РЕЖИМЕ: С СЕМАФОРОМ ===" << std::endl;
+    std::cout << "ЗАПУСК В РЕЖИМЕ: С СЕМАФОРОМ" << std::endl;
 #else
-    std::cout << "=== ЗАПУСК В РЕЖИМЕ: БЕЗ СЕМАФОРА ===" << std::endl;
+    std::cout << "ЗАПУСК В РЕЖИМЕ: БЕЗ СЕМАФОРА" << std::endl;
 #endif
 
-    // Сброс состояния
     g_clubState.currentVisitors = 0;
     g_clubState.maxVisitors = 0;
     g_clubState.servedCount = 0;
@@ -73,16 +66,13 @@ int main() {
         g_clubState.clients[i].timeout = FALSE;
     }
 
-    // Массив дескрипторов потоков (вместо vector)
     HANDLE hVisitors[MAX_CLIENTS];
     HANDLE hObserver;
 
-    // Создание потоков посетителей (T1 - T20)
     for (int i = 0; i < MAX_CLIENTS; ++i) {
         int threadNum = i + 1;
         DWORD priority = THREAD_PRIORITY_NORMAL;
 
-        // Установка приоритетов согласно заданию
         if (threadNum >= 1 && threadNum <= 8) {
             priority = THREAD_PRIORITY_NORMAL;
         }
@@ -99,25 +89,19 @@ int main() {
         }
     }
 
-    // Создание потока наблюдателя (T21)
     hObserver = CreateThread(NULL, 0, ObserverThreadProc, NULL, 0, NULL);
     if (hObserver) {
         SetThreadPriority(hObserver, THREAD_PRIORITY_LOWEST);
     }
 
-    // Ожидание завершения всех посетителей
     WaitForMultipleObjects(MAX_CLIENTS, hVisitors, TRUE, INFINITE);
 
-    // Сигнализируем наблюдателю, что все закончили
     g_bAllVisitorsFinished = TRUE;
 
-    // Ожидание завершения наблюдателя
     WaitForSingleObject(hObserver, INFINITE);
 
-    // Вывод итоговой статистики (Доп. задание)
     PrintFinalStats();
 
-    // Очистка ресурсов
     if (g_hSemaphore) CloseHandle(g_hSemaphore);
     CloseHandle(hObserver);
     for (int i = 0; i < MAX_CLIENTS; ++i) {
@@ -125,18 +109,14 @@ int main() {
     }
     DeleteCriticalSection(&g_csState);
 
-    std::cout << "\nНажмите любую клавишу для выхода..." << std::endl;
-    std::cin.get();
-
     return 0;
 }
 
-// --- Поток посетителя ---
+// Поток посетителя
 DWORD WINAPI VisitorThreadProc(LPVOID lpParam) {
     int clientId = (int)(intptr_t)lpParam;
     int index = clientId - 1;
 
-    // Фиксация прихода
     EnterCriticalSection(&g_csState);
     g_clubState.clients[index].threadId = GetCurrentThreadId();
     g_clubState.clients[index].arriveTick = GetTickCount();
@@ -149,11 +129,11 @@ DWORD WINAPI VisitorThreadProc(LPVOID lpParam) {
 #if USE_SEMAPHORE
     waitResult = WaitForSingleObject(g_hSemaphore, WAIT_TIMEOUT_MS);
 #else
-    // Эмуляция отсутствия семафора (просто ждем немного)
     Sleep(rand() % 1000);
 #endif
 
     if (waitResult == WAIT_OBJECT_0) {
+
         // Место получено
         EnterCriticalSection(&g_csState);
         g_clubState.clients[index].startTick = GetTickCount();
@@ -164,11 +144,10 @@ DWORD WINAPI VisitorThreadProc(LPVOID lpParam) {
         }
         LeaveCriticalSection(&g_csState);
 
-        // Работа за компьютером (2000-5000 мс)
+        // Работа за компьютером
         int workTime = MIN_SERVICE_TIME + rand() % (MAX_SERVICE_TIME - MIN_SERVICE_TIME + 1);
         Sleep(workTime);
 
-        // Освобождение места
         EnterCriticalSection(&g_csState);
         g_clubState.clients[index].endTick = GetTickCount();
         g_clubState.currentVisitors--;
@@ -180,7 +159,6 @@ DWORD WINAPI VisitorThreadProc(LPVOID lpParam) {
 #endif
     }
     else if (waitResult == WAIT_TIMEOUT) {
-        // Таймаут ожидания
         EnterCriticalSection(&g_csState);
         g_clubState.clients[index].timeout = TRUE;
         g_clubState.timeoutCount++;
@@ -190,9 +168,9 @@ DWORD WINAPI VisitorThreadProc(LPVOID lpParam) {
     return 0;
 }
 
-// --- Поток наблюдателя ---
+// Поток наблюдателя
 DWORD WINAPI ObserverThreadProc(LPVOID lpParam) {
-    std::cout << "\n--- Наблюдатель запущен ---" << std::endl;
+    std::cout << "\nНаблюдатель запущен" << std::endl;
     std::cout << "Время     | Занято | Обслужено | Таймауты" << std::endl;
 
     while (!g_bAllVisitorsFinished) {
@@ -204,20 +182,18 @@ DWORD WINAPI ObserverThreadProc(LPVOID lpParam) {
         LONG timeouts = g_clubState.timeoutCount;
         LeaveCriticalSection(&g_csState);
 
-        // Вывод через std::cout с ручным форматированием
         std::cout << GetTickCount() << "   |   "
             << curr << "      |   "
-            << served << "         |   "
+            << served << "     |   "
             << timeouts << std::endl;
     }
 
-    std::cout << "--- Наблюдатель завершил работу ---" << std::endl;
+    std::cout << "Наблюдатель завершил работу" << std::endl;
     return 0;
 }
 
-// --- Вывод итоговой статистики (Доп. задание) ---
 void PrintFinalStats() {
-    std::cout << "\n=== ИТОГОВАЯ СТАТИСТИКА ===" << std::endl;
+    std::cout << "\nИТОГОВАЯ СТАТИСТИКА" << std::endl;
 
     LONG totalWaitTime = 0;
     LONG totalServiceTime = 0;
